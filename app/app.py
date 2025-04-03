8f0d94d96126418dd161be077b46992cca63cf3f
Solangie12 <solangie.santana12@gmail.com>
2025-04-03 18:19:40 -0500

    .

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Request
 import uvicorn
-from app.controlador.PatientCrud import GetPatientById,WritePatient,GetPatientByIdentifier,ReadServiceRequest,get_service_request
+from app.controlador.PatientCrud import GetPatientById,WritePatient,GetPatientByIdentifier,ReadServiceRequest
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
